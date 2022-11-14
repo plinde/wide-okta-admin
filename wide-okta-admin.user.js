3ec1d6dbfc64e00991bd9a8c193f595ea2b06550
Peter Linde <plinde@users.noreply.github.com>
2022-11-14 11:50:16 -0500

    the script

---
+++ wide-okta-admin.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Wide Okta Admin
+// @version      1.0
+// @description  Script for automatically widening Okta's Admin console
+// @author       plinde
+// @match        https://elasticgov-admin.okta.com/*
+// @match        https://elastic-admin.okta.com/*
+// @grant        none
+// @homepageURL https://github.com/plinde/wide-okta-admin
+// ==/UserScript==
+
+var styleSheet = "" +
+"div#content {" +
+  "width: 90%!important;" +
+"}" +
+"";
+
+(function () {
+  var s = document.createElement('style');
+  s.type = "text/css";
+  s.innerHTML = styleSheet;
+  (document.head || document.documentElement).appendChild(s);
+})();
+
